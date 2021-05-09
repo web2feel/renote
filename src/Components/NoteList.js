6656775494110bf8c791b1bd00d2877ed24f993c
Jinson Abraham <web2feel@gmail.com>
2021-05-09 19:44:18 +0530

    Readme changes

--- src/Components/NoteList.js
+++ src/Components/NoteList.js
@@ -30,7 +30,7 @@ function NoteList() {
   return (
     
     <div className="max-w-screen-xl mx-auto flex">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full">
         {posting && <NoteForm />}
         {data &&
           data.map((item) => {
